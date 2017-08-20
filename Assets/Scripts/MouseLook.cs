f79548d376cfa926ba1bc8e0257cc907ada86b99
Shiftyyy <jakeroppa@googlemail.com>
2017-08-20 23:15:36 +0100

    Player Mouse Locking
    Implemented mouse lock toggle for future esc menu

--- Assets/Scripts/MouseLook.cs
+++ Assets/Scripts/MouseLook.cs
@@ -7,15 +7,29 @@ public class MouseLook : MonoBehaviour {
     Vector2 smoothV;
     public float sensitivity = 5.0f;
     public float smoothing = 2.0f;
+    private bool cursorLocked = true;
     GameObject Player;
 
 	// Use this for initialization
 	void Start () {
         Player = this.transform.parent.gameObject;
+        Cursor.lockState = CursorLockMode.Locked;
 	}
 	
 	// Update is called once per frame
 	void Update () {
+        if (Input.GetKeyDown("escape"))
+        {
+            toggleCursorLock();
+        }
+        if (cursorLocked)
+        {
+            moveCamera();
+        }
+	}
+
+    private void moveCamera()
+    {
         var md = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y"));
 
         md = Vector2.Scale(md, new Vector2(sensitivity * smoothing, sensitivity * smoothing));
@@ -26,5 +40,19 @@ public class MouseLook : MonoBehaviour {
 
         transform.localRotation = Quaternion.AngleAxis(-mouseLook.y, Vector3.right);
         Player.transform.localRotation = Quaternion.AngleAxis(mouseLook.x, Player.transform.up);
-	}
+    }
+
+    private void toggleCursorLock()
+    {
+        if(cursorLocked)
+        {
+            Cursor.lockState = CursorLockMode.None;
+            cursorLocked = false;
+        }
+        else
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+            cursorLocked = true;
+        }
+    }
 }
